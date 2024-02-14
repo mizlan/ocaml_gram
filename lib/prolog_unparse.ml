b2d75b37108009aa6e8829108958a957fa4006d0
Michael Lan <michaellan202@gmail.com>
2024-02-13 17:59:38 -0800

    fix: Change name to make it more readable

--- lib/prolog_unparse.ml
+++ lib/prolog_unparse.ml
@@ -8,42 +8,42 @@ let pp_pl_list_nb pp =
   CCList.pp ~pp_start:(return "[@,") ~pp_stop:(return "]") pp
 
 let rec pp_expr f = function
-  | EVar v -> fprintf f "ovar(%a)" string v
-  | EInt i -> fprintf f "oint(%a)" int i
-  | EBool b -> fprintf f "obool(%a)" bool b
+  | EVar v -> fprintf f "o_var(%a)" string v
+  | EInt i -> fprintf f "o_int(%a)" int i
+  | EBool b -> fprintf f "o_bool(%a)" bool b
   | EIfThenElse (c, a, b) ->
-      fprintf f "@[oif(@,%a,@ %a,@ %a@,)@]" pp_expr c pp_expr a pp_expr b
-  | EList l -> fprintf f "@[<hv 2>olist(@,%a@,)@]" (pp_pl_list pp_expr) l
-  | ETuple t -> fprintf f "@[<hv 2>otup(%a)@]" (pp_pl_list pp_expr) t
+      fprintf f "@[o_if(@,%a,@ %a,@ %a@,)@]" pp_expr c pp_expr a pp_expr b
+  | EList l -> fprintf f "@[<hv 2>o_list(@,%a@,)@]" (pp_pl_list pp_expr) l
+  | ETuple t -> fprintf f "@[<hv 2>o_tup(%a)@]" (pp_pl_list pp_expr) t
   | EAbs (pl, e) ->
-      fprintf f "@[<hv 2>@[<hv 2>ofun(%a@],@ %a)@]" (pp_pl_list pp_patt) pl
+      fprintf f "@[<hv 2>@[<hv 2>o_fun(%a@],@ %a)@]" (pp_pl_list pp_patt) pl
         pp_expr e
   | EBop (a, o, b) ->
-      fprintf f "@[obop(@,%a,@ %a,@ %a@,)@]" pp_expr a pp_bop o pp_expr b
+      fprintf f "@[o_bop(@,%a,@ %a,@ %a@,)@]" pp_expr a pp_bop o pp_expr b
   | ECons (x, xs) ->
-      fprintf f "@[<hv 2>ocons(@,%a,@ %a@,)@]" pp_expr x pp_expr xs
+      fprintf f "@[<hv 2>o_cons(@,%a,@ %a@])" pp_expr x pp_expr xs
   | ELet (r, p, e, b) ->
-      fprintf f "@[<hov 2>olet(%a,@ %a,@ %a,@ %a@,)@]" bool r pp_patt p pp_expr
+      fprintf f "@[<hov 2>o_let(%a,@ %a,@ %a,@ %a@,)@]" bool r pp_patt p pp_expr
         e pp_expr b
   | EMatch (e, pl) ->
-      fprintf f "@[<v 2>omatch(%a, %a)@]" pp_expr e
+      fprintf f "@[<v 2>o_match(%a, %a)@]" pp_expr e
         (pp_pl_list (pp_match pp_patt pp_expr))
         pl
   | EApp (fn, xs) ->
-      fprintf f "@[<hv 2>oapp(%a,@ %a)@]" pp_expr fn (CCList.pp pp_expr) xs
+      fprintf f "@[<hv 2>o_app(%a,@ %a)@]" pp_expr fn (CCList.pp pp_expr) xs
 
-and pp_match p e f (a, b) = fprintf f "@[<hv 2>obranch(%a,@ %a)@]" p a e b
+and pp_match p e f (a, b) = fprintf f "@[<hv 2>o_branch(%a,@ %a)@]" p a e b
 
 and pp_patt f = function
-  | PVar v -> fprintf f "ovar(%a)" string v
-  | PInt i -> fprintf f "oint(%a)" int i
-  | PBool b -> fprintf f "obool(%a)" bool b
-  | PUnderscore -> fprintf f "o_"
-  | PTuple t -> fprintf f "@[<hv 2>otup(@,%a@,)@]" (pp_pl_list pp_patt) t
-  | POr (a, b) -> fprintf f "@[oor(%a,@ %a)@]" pp_patt a pp_patt b
+  | PVar v -> fprintf f "o_var(%a)" string v
+  | PInt i -> fprintf f "o_int(%a)" int i
+  | PBool b -> fprintf f "o_bool(%a)" bool b
+  | PUnderscore -> fprintf f "o_wild"
+  | PTuple t -> fprintf f "@[<hv 2>o_tup(@,%a@,)@]" (pp_pl_list pp_patt) t
+  | POr (a, b) -> fprintf f "@[o_or(%a,@ %a)@]" pp_patt a pp_patt b
   | PCons (x, xs) ->
-      fprintf f "@[<hv 2>ocons(@,%a,@ %a@,)@]" pp_patt x pp_patt xs
-  | PList l -> fprintf f "@[olist(%a)@]" (pp_pl_list pp_patt) l
+      fprintf f "@[<hv 2>o_cons(@,%a,@ %a@])" pp_patt x pp_patt xs
+  | PList l -> fprintf f "@[o_list(%a)@]" (pp_pl_list pp_patt) l
 
 and pp_bop f = function
   | BEq -> fprintf f "%a" string_quoted "="
