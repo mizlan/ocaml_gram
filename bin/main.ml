3f92302a0fff3665e287f62be56308e12cdc9752
Michael Lan <michaellan202@gmail.com>
2024-02-13 18:12:46 -0800

    feat: Add a newline in between parse output

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,12 @@
 open Ocaml_gram
 
 let () =
-  Format.set_margin 120;
+  Format.set_margin 80;
   let lexbuf = Lexing.from_channel stdin in
   try
     let ast = Parser.main Lexer.token lexbuf in
     ast |> Syntax.show_expr |> print_endline;
+    print_newline ();
     ast |> Prolog_unparse.unparse
   with Parser.Error ->
     Printf.printf "At offset %d: unexpected character."
